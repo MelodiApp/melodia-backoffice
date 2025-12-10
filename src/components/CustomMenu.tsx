9cc061accc7d7cb4922b12ee9f1fe7a726d4d52f
Valen1611 <vschneider@fi.uba.ar>
2025-12-10 18:38:21 -0300

    add artists tab

--- src/components/CustomMenu.tsx
+++ src/components/CustomMenu.tsx
@@ -7,6 +7,7 @@ export const CustomMenu = (props: any) => (
     <MenuItemLink to="/" primaryText="Dashboard" leftIcon={<DashboardIcon />} />
     <MenuItemLink to="/users" primaryText="Usuarios" leftIcon={<People />} />
     <MenuItemLink to="/catalog" primaryText="Catálogo" leftIcon={<LibraryMusic />} />
+    <MenuItemLink to="/artists" primaryText="Artistas" leftIcon={<People />} />
     <MenuItemLink to="/metrics/users" primaryText="Métricas (Usuarios)" leftIcon={<Analytics />} />
   {/* Settings menu removed per request */}
   </Menu>
